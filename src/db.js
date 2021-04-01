1e901988cc4712859e558e041c48fa8b59e1ba41
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-04-01 12:32:03 +0200

    comments watching lecture video

--- src/db.js
+++ src/db.js
@@ -4,18 +4,18 @@ const db = {
       id: "1",
       name: "Anna",
       email: "anna@codegram.com",
-      age: 32
+      age: 32,
     },
     {
       id: "2",
       name: "John",
-      email: "john@gmail.com"
+      email: "john@gmail.com",
     },
     {
       id: "3",
       name: "Michael",
-      email: "michael@gmail.com"
-    }
+      email: "michael@gmail.com",
+    },
   ],
   posts: [
     {
@@ -23,23 +23,43 @@ const db = {
       title: "This is my first blog post",
       body: "Welcome to my first post",
       published: true,
-      author: "1"
+      author: "1",
     },
     {
       id: "2",
       title: "Another blog post",
       body: "This is my second blog post",
       published: false,
-      author: "1"
+      author: "1",
     },
     {
       id: "3",
       title: "Draft blog post title",
       body: "",
       published: false,
-      author: "2"
-    }
-  ]
+      author: "2",
+    },
+  ],
+  comments: [
+    {
+      id: "1",
+      text: "Here is a comment",
+      post: "2",
+      author: "3",
+    },
+    {
+      id: "2",
+      text: "This post is great!",
+      post: "2",
+      author: "1",
+    },
+    {
+      id: "3",
+      text: "Not very good",
+      post: "3",
+      author: "2",
+    },
+  ],
 };
 
 export { db as default };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,67 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 import db from "./db";
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+    posts: [Post!]!
+    user(id: ID!): User
+    post(id: ID!): Post
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+    posts: [Post!]!
+    comments: [Comment!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    post: Post!
+    author: User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    posts() {
+      return db.posts;
+    },
+    users() {
+      return db.users;
+    },
+    user(_, args) {
+      return db.users.find((user) => user.id === args.id); //finding the user by passing an ID
+    },
+    post(_, args) {
+      return db.posts.find((post) => post.id === args.id);
+    },
+  },
+
+  Post: {
+    author(post) {
+      return db.users.find((user) => user.id === post.author);
+    },
+  },
+  User: {
+    posts(user) {
+      return db.posts.filter((post) => post.author === user.id);
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -9,49 +71,3 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
-
-const typeDefs = gql`
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    author: ID!
-    published: Boolean!
-  }
-
-  type Query {
-    users: [User!]!
-    posts: [Post!]!
-  }
-
-  type Query {
-    users: [User!]!
-    posts: [Post!]!
-    user(id: ID!): User
-  }
-`;
-const resolvers = {
-  Query: {
-    users() {
-      return db.users;
-    },
-
-    posts() {
-      return db.posts;
-    },
-  },
-};
-const resolvers = {
-  Query: {
-    user(_, args) {
-      return db.users.find((user) => user.id === args.id);
-    },
-  },
-};
