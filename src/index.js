fbafbfa2417e09670c16050138f6f56c32ef5723
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-04-01 22:46:53 +0200

    subscription, something is not working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql, PubSub } from "apollo-server";
 import db from "./db";
 import uuid from "uuid/v4";
+
+const pubsub = PubSub;
 
 const typeDefs = gql`
   type Query {
@@ -13,6 +15,12 @@ const typeDefs = gql`
   type Mutation {
     createUser(data: CreateUserInput!): User!
     createPost(data: CreatePostInput!): Post!
+    createComment(data: CreateCommentInput!): Comment!
+  }
+
+  type Subscription {
+    post: Post!
+    comment(postId: ID!): Comment!
   }
 
   input CreatePostInput {
@@ -26,6 +34,12 @@ const typeDefs = gql`
     name: String!
     email: String!
     age: Int
+  }
+
+  input CreateCommentInput {
+    text: String!
+    post: ID!
+    author: ID!
   }
 
   type User {
@@ -93,7 +107,42 @@ const resolvers = {
         author: data.author,
       };
       db.posts.push(post);
+      pubsub.publish("post", { post });
       return post;
+    },
+
+    createComment(_, { data }) {
+      const author = db.users.find((user) => user.id === data.author);
+      if (!author) {
+        throw new Error(`No user found for ID ${data.author}`);
+      }
+      const post = db.post.find((post) => post.id === data.post);
+      if (!post) {
+        throw new Error(`No post found for ID ${data.post}`);
+      }
+      const comment = {
+        id: uuid(),
+        text: data.text,
+        post: data.post,
+        author: data.author,
+      };
+      db.comments.push(comment);
+      pubsub.publish(`comment ${data.post}`, { comment });
+      return comment;
+    },
+  },
+
+  Subscription: {
+    post: {
+      //it has a level more respect Query and Mutation
+      subscribe() {
+        return pubsub.asyncIterator(["post"]); //method that creates a channel
+      },
+    },
+    comment: {
+      subscribe(_, { postId }) {
+        return pubsub.asyncIterator(`comment ${postId}`);
+      },
     },
   },
 
